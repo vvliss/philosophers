7f6ea8306940ede090abe859da05c2d5c3e21735
Wiktoria Lisson <wilisson@3-G-10.42heilbronn.de>
2025-12-30 18:51:34 +0100

    Initial commit

---
+++ philo.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wilisson <wilisson@student.42heilbronn.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/30 14:00:39 by wilisson          #+#    #+#             */
+/*   Updated: 2025/12/30 15:12:36 by wilisson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdlib.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <sys/time.h>
+
+typedef struct s_table
+{
+	long			num_philos;
+	long			time_to_die;
+	long			time_to_eat;
+	long			time_to_sleep;
+	long			must_eat_count;
+	long long		start_time;
+	bool			simulation_running;
+	pthread_mutex_t	sim_mutex;
+	pthread_mutex_t	print_mutex;
+	pthread_mutex_t	*forks;
+	t_philo			*philos;
+}                   t_table;
+
+typedef struct s_philo
+{
+	int				id;
+	int				eat_count;
+	long long		last_meal_time;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+	pthread_mutex_t	meal_mutex;
+	t_table			    *table;
+}                       t_philo;
+
+
+#endif
